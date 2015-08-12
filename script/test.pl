65b0912a5f42b034c99f0ff9368b68fd51000329
Rutger Vos <rutgeraldo@gmail.com>
2015-08-12 21:27:52 +0200

    added comments

--- script/test.pl
+++ script/test.pl
@@ -8,28 +8,28 @@ my $size = 44_100;
 
 # create an LFO scaled between a3 and a4 in a 1-second cycle
 my $pitch_osc = Audio::Synth::Modular::Oscillator->new(
-	frequency => 1 / $size,
-	shape     => 'sine',
-	size      => $size,
-	min       => a3,
-	max       => a4,
+	frequency => 1 / $size, # i.e. wave length
+	shape     => 'sine',    # wave shape
+	size      => $size,     # total number of samples
+	min       => a3,        # minimum amplitude
+	max       => a4,        # maximum amplitude
 );
 
 # create a sine wave oscillator at whose pitch is modulated
 my $osc = Audio::Synth::Modular::Oscillator->new(
-	frequency => $pitch_osc,
-	shape     => 'saw',
-	size      => $size,
+	frequency => $pitch_osc, # wave length can be a number, a piddle, or a Modulator
+	shape     => 'saw',      # wave shape
+	size      => $size,      # total number of samples
 );
 
 # create an ADSR envelope
 my $amp_env = Audio::Synth::Modular::Envelope->new(
-	attack   => 0.01,
-	decay    => 0.1,
-	sustain  => 0.3,
-	release  => 1.0,
-	level    => 0.2,
-	size     => $size,
+	attack    => 0.01,
+	decay     => 0.1,
+	sustain   => 0.3,
+	release   => 1.0,
+	level     => 0.5,
+	size      => $size,
 );
 
 # create a filter whose frequency is modulated by the ADSR
@@ -44,6 +44,7 @@ my $out = Audio::Synth::Modular::FileWriter->new;
 # chain the signal
 $osc->to($amp_env)->to($flt)->to($out);
 
+# write 10 filter frequency levels to files
 for my $i ( 1 .. 10 ) {
 	$flt->frequency( a4 * $i );
 	$out->path( "deleteme${i}.au" );
